c07e91658626d6c54ae85006c5272cbda4cfb5a0
Guillermo De La Cruz <guillermo.dlco@outlook.com>
2019-07-15 00:02:57 -0500

    first screan

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// void main(){
+//   //Para ejecutar la clase con el widget
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text!'),
+      ),
+    );
+  }
+}
